0000099ba23f3985cb81d113d225515082da8c09
Devin Costello <86488145+DevinCostello@users.noreply.github.com>
2023-02-10 12:33:47 -0500

    update cors policy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }))
 
 //CORS middleware
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    res.header("Access-Control-Allow-Origin", "https://mern-rtk-store.onrender.com"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
     
